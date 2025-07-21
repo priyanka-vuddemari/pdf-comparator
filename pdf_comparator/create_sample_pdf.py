b86518f54c43f961547b0b2a954cadc8013e83ad
Priyanka Vuddemari <58094868+priyanka-vuddemari@users.noreply.github.com>
2025-07-21 18:33:25 +0000

    updated create pdf python script

--- pdf_comparator/create_sample_pdf.py
+++ pdf_comparator/create_sample_pdf.py
@@ -7,10 +7,10 @@ pdf.set_font("Arial", size=12)
 pdf.multi_cell(0, 10, """Proposal of Insurance
 Date Quoted: [07/01/2023]
 Prepared For: Presented By:
-[Rajendra Babu]
-[Rajendra]
-Dear [Rajendra]
+[Sai baba]
+[Sai]
+Dear [Sai]
 I am pleased to present you with a proposal of coverage designed to meet the individual needs of your 
 business. This proposal letter summarizes the coverages, limits and premiums associated with the policy 
 that you are considering.""")
-pdf.output("pdf_comparator/sample.pdf")
+pdf.output("sample.pdf")
